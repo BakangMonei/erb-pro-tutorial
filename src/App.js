0743ea5c0b10434eec08e34756a33e0703e00660
BakangMonei <bakangmonei2@gmail.com>
2024-01-18 12:05:38 +0200

    Proooo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PocketBase from "pocketbase";
 
 function App() {
+  const [data, setData] = useState([
+    { registrationNumber: "", certificateNumber: "" },
+  ]);
+
+  useEffect(() => {
+    const pb = new PocketBase("https://certificates.erb.org.bw/db");
+
+    // Fetch data from the collection using PocketBase
+    const fetchData = async () => {
+      try {
+        const response = await pb.collection("certificates").getFullList({
+          fields: "registrationNumber, certificateNumber",
+        });
+        console.log("Data fetched successfully:", response);
+        setData(response || []); // Ensure that response.records is defined
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data[0].certificateNumber}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Registration Number</th>
+            <th>Certificate Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr>
+              <td>{item.registrationNumber}</td>
+              <td>{item.certificateNumber}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
