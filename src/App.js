dafef9c0316354555c2ddcfb3b84c3258179e5d9
Withus Chivia <75859835+Prochivs@users.noreply.github.com>
2026-03-19 00:23:35 +0200

    Remove PocketBase and add Hello World header
    Removed PocketBase integration and data fetching logic. Added a simple 'Hello World' header.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import React, { useState, useEffect } from "react";
-import PocketBase from "pocketbase";
+
 
 function App() {
   const [data, setData] = useState([
     { registrationNumber: "", certificateNumber: "" },
   ]);
 
-  useEffect(() => {
-    const pb = new PocketBase("https://certificates.erb.org.bw/db");
-
-    // Fetch data from the collection using PocketBase
-    const fetchData = async () => {
-      try {
-        const response = await pb.collection("certificates").getFullList({
-          fields: "registrationNumber, certificateNumber",
-        });
-        console.log("Data fetched successfully:", response);
-        setData(response || []); // Ensure that response.records is defined
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+ 
 
   return (
     <div className="App">
-      {/* <h1>{data[0].certificateNumber}</h1>/ */}
-      <table>
-        <thead>
-          <tr>
-            <th>Registration Number</th>
-            <th>Certificate Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr>
-              <td>{item.registrationNumber}</td>
-              <td>{item.certificateNumber}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+ <h1>Hello World</h1>/ 
+    
     </div>
   );
 }
